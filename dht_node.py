baf5525c3b23ef56f17b7399f1524517a8ba3f4c
Vic Chimenti <chimenti@seattleu.edu>
2018-11-29 21:13:23 -0800

    Update dht_node.py

--- dht_node.py
+++ dht_node.py
@@ -21,29 +21,6 @@ import argparse         # for parsing command line arguments
 #   ***************     function definitions     ***************   #
 
 
-# get the hostname
-# def getHost() :
-#     try :
-#         h = socket.gethostname()
-#     except AttributeError :
-#         error_message = "ERROR Failed to Get Hostname"
-#         print (error_message)
-#         sys.exit ("Exiting Program")
-#
-#     return h
-#
-#
-# # get the host IP number
-# def getIP(h) :
-#     try :
-#         h_ip = socket.gethostbyname(h)
-#     except AttributeError :
-#         error_message = "ERROR Failed to Get Host IP Number"
-#         print (error_message)
-#         sys.exit ("Exiting Program")
-#
-#     return h_ip
-
 # # define the size of the table
 # def distance(a, b):
 #     return a^b
@@ -88,22 +65,25 @@ print ('hostfile : ' + str(args.hostfile))
 print ('linenum : ' + str(args.linenum))
 
 
-# open file and assign
+
+
+# open file and assign to list
 with open (args.hostfile[0], 'r') as file :
     content = file.readlines()
-#host_addr = content[args.linenum[0]]
+
+# split list and assign address and port number
 host_addr, host_port_str = content[args.linenum[0]].split()
 host_port = int(host_port_str)
 
-# get the local host and ip address
-# host = getHost()
-# host_ip = getIP(host)
+
 
 
 # create a udp socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((host_addr, host_port))
 print ("Listening on Port : " + str(host_port))
+
+
 
 
 # listen for communication
